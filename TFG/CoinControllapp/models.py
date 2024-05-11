4b7cd3c2eaf979c9db4d9b7ebd409656130c846a
Ismael B B <ismbou9@alu.edu.gva.es>
2024-05-11 18:19:10 +0200

    Ingresos a medias

--- TFG/CoinControllapp/models.py
+++ TFG/CoinControllapp/models.py
@@ -20,3 +20,14 @@ class Gasto(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Ingreso(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    fecha = models.DateField()
+    fechaFin = models.DateField(blank=True, null=True)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Ingreso de {self.monto} ({self.categoria}) desde {self.fecha_inicio} hasta {self.fecha_fin if self.fecha_fin else 'presente'}"
